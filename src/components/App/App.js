4f3c57a235dbe73d55328c87fbbf8003020fad3d
Petr <petr.podo@gmail.com>
2021-06-07 13:19:32 +0300

    fix path

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import StartPage from "../StartPage/StartPage";
 import Game from "../Game/Game";
@@ -6,31 +6,23 @@ import './App.css';
 
 export const GameContext = React.createContext(null);
 function App() {
-    const [fieldSize, setFieldSize] = useState('3');
 
     return (
-        <GameContext.Provider
-            value={{
-                fieldSize,
-                setFieldSize,
-            }}
-        >
-            <div className='App'>
-                <Router>
-                    <Switch>
-                        <Route exact path='/'>
-                            <StartPage />
-                        </Route>
-                        <Route exact path='/game'>
-                            <Game />
-                        </Route>
-                        <Route path='*'>
-                            <Redirect to='/' />
-                        </Route>
-                    </Switch>
-                </Router>
-            </div>
-        </GameContext.Provider>
+        <div className='App'>
+            <Router basename='/tic-tac-toe/'>
+                <Switch>
+                    <Route exact path='/'>
+                        <StartPage />
+                    </Route>
+                    <Route exact path='/game'>
+                        <Game />
+                    </Route>
+                    <Route path='*'>
+                        <Redirect to='/' />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
     );
 }
 
